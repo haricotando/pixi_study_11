a7046b1c11a7791a867d841ae14936030b218715
Masaru ANDO <haricotando@gmail.com>
2025-01-15 11:49:25 +0900

    PCView用のQR 生成の同期／非同期の修正

--- QRView.js
+++ QRView.js
@@ -1,39 +1,45 @@
 import GraphicsHelper from "./class/helper/GraphicsHelper.js";
-import Utils from "./class/util/Utils.js";
 import { dataProvider, dp } from "./dataProvider.js";
 
 export class QRView extends PIXI.Container {
     
     constructor() {
         super();
+
         this.sortableChildren = true;
-
+        this.frame = this.addChild(GraphicsHelper.exDrawRect(0, 0, dp.stageRect.width, dp.stageRect.height, false, {color:0xFFFFFF}));
+        this.init();
+    }
+    
+    init(){
+        let bLoaded = false;
         const qr = qrcode(0, 'M');
-        qr.addData(window.location.href);
+        const url = window.location.href;
+        qr.addData(url);
         qr.make();
-        const qrDataURL = qr.createDataURL(10, 0);
+        const qrDataURL = qr.createDataURL(20, 0);
         const texture = PIXI.Texture.from(qrDataURL);
-        
-        const qrContainer = new PIXI.Sprite(texture);
-        qrContainer.x = dp.stageRect.halfWidth - qrContainer.width / 2;
-        qrContainer.y = dp.stageRect.halfHeight - qrContainer.width / 2;
-        this.addChild(qrContainer);
-        qrContainer.zIndex = 10;
-        qrContainer.alpha = 0;
-        
-        const bgContainer = this.addChild(new PIXI.Container());
-        const bg = GraphicsHelper.exDrawRect(0, 0, qrContainer.width + 20, qrContainer.width + 20, false, {color:0xFFFFFF});
-        Utils.pivotCenter(bg);
-        bgContainer.addChild(bg);
-        bgContainer.x = dp.stageRect.halfWidth;
-        bgContainer.y = dp.stageRect.halfHeight;
-        bgContainer.alpha = 0;
-        bgContainer.scale.set(1.2);
 
-        gsap.timeline()
-            .to(bgContainer.scale, {x:1, y:1, duration:0.3, ease:'expo.out'})
-            .to(bgContainer, {alpha:1, duration:0.2, ease:'none'}, '<')
-            .to(qrContainer, {alpha:1, duration:0.2, ease:'none'}, '<0.1')
+        const onLoaded = (e) => {
+            if(bLoaded){
+                return false;
+            }
+            bLoaded = true;
 
+            texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
+            const qrSprite = new PIXI.Sprite(texture);
+            this.addChild(qrSprite);
+            qrSprite.width = dp.stageRect.width - 40;
+            qrSprite.height = dp.stageRect.height - 40;
+            qrSprite.position.set(20, 20);
+        }
+        
+        texture.baseTexture.once('loaded', () => {
+            onLoaded();
+        });
+
+        if(texture.width){
+            onLoaded();
+        }
     }
 }
